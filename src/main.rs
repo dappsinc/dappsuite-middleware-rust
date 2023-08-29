cef6aa6ccd9d3b6778d6399fc2c53f3145dfbb03
domsteil <domsteil14@gmail.com>
2023-08-29 16:16:43 -0700

    adding euroc

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,9 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
 
-        // `POST /users` goes to `create_user`
         .route("/send-usdc", post(send_usdc));
+
+        .route("/send-euroc", post(send_euroc));
 
 
     // run our app with hyper
@@ -117,11 +118,80 @@ async fn send_usdc(Json(payload): Json<SendUsdc>) -> Result<T, E> {
 }
 
 
+// Basic Function to Send USDC
+async fn send_euroc(Json(payload): Json<SendUsdc>) -> Result<T, E> {
+
+    let from_wallet: LocalWallet = process.env.PRIVATE_KEY;
+    .parse::<LocalWallet>()
+    .unwrap();
+
+    let to_address = payload.receiver.parse::<Address>()?;
+    let amount = payload.amount;
+
+    let euroc_token_addr = "0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c".parse::<Address>()?;
+
+    // 1. Generate the ABI for the ERC20 contract. This is will define an `ERC20Contract` struct in
+    // this scope that will let us call the methods of the contract.
+    abigen!(
+        ERC20Contract,
+        r#"[
+            function balanceOf(address account) external view returns (uint256)
+            function decimals() external view returns (uint8)
+            function symbol() external view returns (string memory)
+            function transfer(address to, uint256 amount) external returns (bool)
+            event Transfer(address indexed from, address indexed to, uint256 value)
+        ]"#,
+    );
+
+    // 2. Create the contract instance to let us call methods of the contract and let it sign
+    // transactions with the sender wallet.
+    let provider =
+        Provider::<Http>::try_from("https://mainnet.infura.io/v3/" + process.env.INFURA_KEY)?.interval(Duration::from_millis(10u64));
+
+    // Chain ID
+    let chain_id = provider.get_chainid().await?;
+
+    let signer = Arc::new(SignerMiddleware::new(provider, wallet.with_chain_id(chain_id.as_u64())));
+    let contract = ERC20Contract::new(euroc_token_addr, signer);
+
+    // 3. Fetch the decimals used by the contract so we can compute the decimal amount to send.
+    let whole_amount: u64 = amount;
+    let decimals = contract.decimals().call();
+    let decimal_amount = U256::from(whole_amount) * U256::exp10(decimals as usize);
+
+    // 4. Transfer the desired amount of tokens to the `to_address`
+    let tx = contract.transfer(to_address, decimal_amount);
+
+    let pending_tx = tx.send().await?;
+    let _mined_tx = pending_tx.await?;
+
+    println!("Transaction Receipt: {}", serde_json::to_string(&_mined_tx)?);
+
+    // Extract the tx hash for printing
+    let json_str = serde_json::to_string(&_mined_tx)?;
+    let json: Value = serde_json::from_str(&json_str)?;
+
+    if let Some(transaction_hash) = json["transactionHash"].as_str() {
+        println!("\n URL: https://etherscan.io/tx/{}", transaction_hash);
+    } else {
+        println!("Transaction Hash not found");
+    }
+
+    Ok(())
+
+}
+
+
 // a request body type
 
 #[derive(Deserialize, Serialize)]
 
 struct SendUsdc {
+    receiver: String,
+    amount: u64,
+}
+
+struct SendEuroc {
     receiver: String,
     amount: u64,
 }
